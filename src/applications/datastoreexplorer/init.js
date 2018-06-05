0ebbe07e5cfcdd1ca4d5fdb589b02e66a67fee13
infnada <inf.nada@gmail.com>
2018-06-05 09:42:38 +0200

    Datastore Explorer APP - Watch for datastores changes

--- src/applications/datastoreexplorer/init.js
+++ src/applications/datastoreexplorer/init.js
@@ -19,11 +19,29 @@ var datastoreexplorerApp = angular.module('datastoreexplorerApp', []);
             templateUrl: 'applications/datastoreexplorer/modals/DatastoreSelectable.html',
             size: 'sm',
             controllerAs: 'dsmC',
-            controller: ['title', 'connectionsFactory', '$uibModalInstance', function (title, connectionsFactory, $uibModalInstance) {
+            controller: ['$scope', 'title', 'connectionsFactory', '$uibModalInstance', function ($scope, title, connectionsFactory, $uibModalInstance) {
                 var _this = this;
-                var connections = connectionsFactory.getConnectionByCategory('virtual');
                 this.title = title;
                 this.datastores = [];
+
+                $scope.$watch(function () {
+                    return connectionsFactory.getConnectionByCategory('virtual');
+                }, function (newValue) {
+                    angular.forEach(newValue, function (connection) {
+                        angular.forEach(connection.datastores, function (datastore) {
+                            _this.datastores.push({
+                                name: datastore.name,
+                                id: datastore.obj.name,
+                                credential: connection.credential,
+                                host: connection.host,
+                                port: connection.port,
+                                datacenter: connection.datacenters[0].datacenter // TODO: check datacenter per datastore
+                                                                                 // (not always will be the 1st [0]
+                                                                                 // datacenter)
+                            });
+                        });
+                    });
+                });
 
 	            this.close = function () {
 		            $uibModalInstance.close();
@@ -34,20 +52,6 @@ var datastoreexplorerApp = angular.module('datastoreexplorerApp', []);
                     $uibModalInstance.close(_this.selectedDatastore);
                 };
 
-                angular.forEach(connections, function (connection) {
-                    angular.forEach(connection.datastores, function (datastore) {
-                        _this.datastores.push({
-                            name: datastore.name,
-                            id: datastore.obj.name,
-                            credential: connection.credential,
-                            host: connection.host,
-                            port: connection.port,
-                            datacenter: connection.datacenters[0].datacenter // TODO: check datacenter per datastore
-                                                                             // (not always will be the 1st [0]
-                                                                             // datacenter)
-                        });
-                    });
-                });
             }]
         });
 
