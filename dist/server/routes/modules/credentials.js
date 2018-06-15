589624753e45060cf3b653e21030780e6b15ec54
infnada <inf.nada@gmail.com>
2018-06-16 00:55:44 +0200

    Backend - Lots of routes rebuilds - Created basics for user login
    Frontend
    - CSS additions for Login page
    - Removed desktop background image
    - Added angular-cookies dependency
    - Some error handlers changes
    
    Infrastructure Manager APP
    - Now connection is deleted if it fails to connect to remote host or with invalid credentials
    - Some minor changes

---
+++ dist/server/routes/modules/credentials.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var Promise = require("bluebird");
+
+exports.getCredential = function (credential) {
+    var credentials = require('read-config')(path.join(__dirname, '../../filesystem/root/credentials.json'));
+
+    credential = credentials.saved_credentials.filter(function (obj) {
+        return obj.uuid === credential;
+    })[0];
+
+    if (!credential) return Promise.reject('Invalid credential');
+
+    return Promise.resolve(credential);
+};
