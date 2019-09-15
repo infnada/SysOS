18505ac452e01024736777318b595f92bf6a47b4
infnada <inf.nada@gmail.com>
2019-09-15 22:43:26 +0200

    Init

--- src/backend/app/routes/modules/node/monitor/query.ts
+++ src/backend/app/routes/modules/node/monitor/query.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import {Decimal} from 'decimal.js';
 
 import {ApiGlobalsModule} from '../../../api/api-globals';
 import {DatabaseMonitorModule, chartsDatabase} from './database';
@@ -30,10 +31,19 @@ export class QueryMonitorModule {
   dbData = [];
   dbMatchingData;
 
+  // Calculate data
+  currentIntervalMax;
+  currentIntervalMin;
+  currentValues = [];
+  currentResult = [];
+  intervalNotFound;
+  firstEntryReturnedTimestamp;
+  lastEntryReturnedTimestamp;
+
   // Returned values
   latestReturnedValues;
-  min = 0;
-  max = 0;
+  min = new Decimal(0);
+  max = new Decimal(0);
 
   constructor(private req: express.Request,
               private res: express.Response) {
@@ -48,60 +58,185 @@ export class QueryMonitorModule {
     return apiGlobals.responseJsonData(data);
   }
 
-  private deleteDimensions() {
+  private deleteDimensionsSetPErcentage() {
 
     // Get latest_values based on current dimensions
     this.latestValues = JSON.parse(JSON.stringify(this.dbData[this.dbData.length - 1]));
     this.latestValues.shift(); // Remove timestamp from data
 
-    if (this.dimensions.length !== 0 && this.dimensions.length !== this.dimensionsNames.length) {
+    if (this.dimensions.length !== 0 || this.options.includes('percentage')) {
       let dimensions_index = [];
+      let currentTotal;
 
-      // Get dimensions index that we want to delete
-      this.dimensionsNames.forEach((obj, i) => {
-        if (!this.dimensions.includes(obj)) dimensions_index.push(i ); // +1 because we need to pass the 'time' dimension
-      });
-
-      // Sort reverse to make sure all data is deleted in order
-      dimensions_index.sort(function(a, b){return b-a});
-
-      // Remove non wanted dimensions
-      dimensions_index.forEach(idx => {
-        this.dimensionsNames.splice(idx, 1);
-        this.dimensionsIds.splice(idx, 1);
-        this.latestValues.splice(idx, 1);
-      });
-
-      // Delete data from based on dimension index
-      this.dbMatchingData.forEach(obj => {
-        dimensions_index.forEach(idx => {
-          obj.splice(idx + 1, 1);  // +1 because we need to pass the 'time' dimension
+      // Some domensions specified could not be correct or not exist in this chart
+      if (this.dimensions.length !== 0) {
+        // Get dimensions index that we want to delete
+        this.dimensionsNames.forEach((obj, i) => {
+          if (!this.dimensions.includes(obj)) dimensions_index.push(i); // +1 because we need to pass the 'time' dimension
         });
+
+        // Sort reverse to make sure all data is deleted in order
+        dimensions_index.sort(function(a, b){return b-a});
+
+        // Remove non wanted dimensions
+        dimensions_index.forEach(idx => {
+          this.dimensionsNames.splice(idx, 1);
+          this.dimensionsIds.splice(idx, 1);
+          this.latestValues.splice(idx, 1);
+        });
+      }
+
+      // Delete data from based on dimension index and set percentage total
+      this.dbMatchingData.forEach(obj => {
+
+        // Used to calculate %
+        if (this.options.includes('percentage')) {
+          currentTotal = obj.reduce((a, b, i) => {
+            if (i > 0) {
+              return new Decimal(a).plus(b);
+            } else {
+              return 0;
+            }
+          }, 0);
+        }
+
+        if (dimensions_index.length !== 0) {
+          dimensions_index.forEach(idx => {
+            obj.splice(idx + 1, 1);  // +1 because we need to pass the 'time' dimension
+          });
+        }
+
+        // Push total at start of the array
+        if (this.options.includes('percentage')) {
+          obj.unshift(currentTotal);
+        }
+
       });
     }
   }
 
-  private async getDatabaseMatchingData(update_every, first_entry_t, last_entry_t, absolute_period_requested): Promise<{
-    firstEntryReturnedTimestamp: number;
-    lastEntryReturnedTimestamp: number;
-    result: any[]
-  }> {
-    if (!this.dbData || this.dbData.length === 0) {
-      return {
-        firstEntryReturnedTimestamp: null,
-        lastEntryReturnedTimestamp: null,
-        result: null
-      };
+  private prepareCurrentValues() {
+    let res;
+    let timeToReturn = (!this.options.includes('ms') && !this.options.includes('milliseconds') ? this.currentIntervalMax / 1000 : this.currentIntervalMax);
+
+    if (this.options.includes('nonzero')) {
+      // TODO delete dimensions that have all values to 0
+      // If all dimensions values are 0, return all dimensions
     }
+    if (this.options.includes('min2max')) {
+      // TODO min2max ???????
+    }
+    if (this.options.includes('null2zero')) {
+      // TODO null2zero ???????
+    }
+    if (this.options.includes('objectrows')) {
+      // TODO
+      /*
+      {
+         "labels": ["time", "guest_nice", "guest", "steal", "softirq", "irq", "user", "system", "nice", "iowait"],
+         "data":[
+          { "time": 1568301750, "guest_nice": 0, "guest": 0, "steal": 0.1538782, "softirq": 2.0604372, "irq": 0, "user": 1.2238275, "system": 14.5363847, "nice": 39.161784, "iowait": 0},
+          { "time": 1568301740, "guest_nice": 0, "guest": 0, "steal": 0.3002563, "softirq": 0.3041003, "irq": 0, "user": 1.3573818, "system": 3.4644972, "nice": 47.999079, "iowait": 0.0502513}
+         ]
+       }
+       */
+    }
+    if (this.options.includes('google_json')) {
+      // TODO [ new Date(2019,8,12,16,23,40), 0, 0, 0.0507614, 0.0510204, 0, 1.3659877, 1.3149827, 0, 0],
+    }
+    // TODO test unaligned
+    if (this.options.includes('match-ids')) {
+      // TODO match-ids
+    }
+    if (this.options.includes('match-names')) {
+      // TODO match-names
+    }
+    if (this.options.includes('showcustomvars')) {
+      // TODO showcustomvars
+    }
+
+    /**
+     * Set current interval found values
+     */
+
+    // If no value found, make sure there is at least one to do the calculations
+    if (this.currentValues.length === 0) {
+      let itemToAdd = 1; // timestamp dimension
+      if (this.options.includes('percentage')) itemToAdd = 2; // sum for average & timestamp
+
+      this.currentValues.push(Array(this.dimensionsNames.length + itemToAdd).fill(0));
+    }
+
+    // Calculate result based on group
+    if (this.group === 'average') {
+      res = this.currentValues.reduce((a, b) => {
+        console.log(a, b);
+        // calculate average
+        return a.map((suba, i) => new Decimal(a[i]).plus(b[i]).dividedBy(this.currentValues.length));
+      });
+
+      // Delete 1st element and get currentTotal = all dimensions sum
+      let currentTotal;
+      if (this.options.includes('percentage')) currentTotal = res.shift();
+
+      res.shift(); // Delete timestamp average
+
+      // Set result as percentages
+      if (this.options.includes('percentage')) {
+        res.forEach((o, i, a) => {
+          a[i] = (currentTotal === 0 ? 0 : new Decimal(a[i]).dividedBy(currentTotal).times(100));
+        });
+      }
+    }
+
+    // Set current value as latest value
+    if (!this.latestReturnedValues) this.latestReturnedValues = res.map((d) => { return new Decimal(d).toSignificantDigits(7); });
+
+    /**
+     * output formats
+     */
+    if (this.format === 'array') {
+      res = new Decimal(res.reduce((a, b) => new Decimal(a).plus(b))).toSignificantDigits(7);
+
+      // Abs if needed
+      if (this.options.includes('abs') || this.options.includes('absolute') || this.options.includes('absolute-sum')) res = Math.abs(res);
+
+      this.currentResult.push(res);
+
+      if (this.max < res) this.max = res;
+      if (this.min > res) this.min = res;
+    }
+
+    if (this.format === 'json') {
+
+      // Abs if needed
+      if (this.options.includes('abs') || this.options.includes('absolute') || this.options.includes('absolute-sum')) res.map(Math.abs);
+
+      res.map(obj => {
+        return new Decimal(obj).toSignificantDigits(7);
+      });
+      this.currentResult.push([timeToReturn, ...res]);
+
+      if (this.max < Decimal.max(...res)) this.max = Decimal.max(...res);
+      if (this.min > Decimal.min(...res)) this.min = Decimal.min(...res);
+    }
+
+    if (!this.firstEntryReturnedTimestamp) this.firstEntryReturnedTimestamp = timeToReturn;
+    this.lastEntryReturnedTimestamp = timeToReturn;
+
+    // Set next interval and reset current values
+    this.currentValues = [];
+  };
+
+  private async getDatabaseMatchingData(update_every, first_entry_t, last_entry_t, absolute_period_requested): Promise<any[]> {
+    if (!this.dbData || this.dbData.length === 0) return null;
 
     let points_requested = this.points;
     let after_requested = this.after;
     let before_requested = this.before;
-    const group_method = this.group;
     const resampling_time_requested = this.gtime;
-    const options = this.options;
 
-    const aligned = !(options.includes('unaligned'));
+    const aligned = !(this.options.includes('unaligned'));
 
     if (!absolute_period_requested) {
       if (before_requested % update_every) {
@@ -122,13 +257,7 @@ export class QueryMonitorModule {
     let duration = before_requested - after_requested;
     let available_points = duration / update_every;
 
-    if (duration <= 0 || available_points <= 0) {
-      return {
-        firstEntryReturnedTimestamp: null,
-        lastEntryReturnedTimestamp: null,
-        result: null
-      };
-    }
+    if (duration <= 0 || available_points <= 0) return null;
 
     // check the number of wanted points in the result
     if (points_requested < 0) points_requested = -points_requested;
@@ -226,13 +355,7 @@ export class QueryMonitorModule {
     // -------------------------------------------------------------------------
     // initialize our result set
 
-    if (!points_wanted) {
-      return {
-        firstEntryReturnedTimestamp: null,
-        lastEntryReturnedTimestamp: null,
-        result: null
-      };
-    }
+    if (!points_wanted) return null;
 
     // -------------------------------------------------------------------------
     // initialize RRDR
@@ -242,8 +365,8 @@ export class QueryMonitorModule {
       update_every: group * update_every,
       before: Math.floor(before_wanted * 1000),
       after: Math.floor(after_wanted * 1000),
+      points_wanted: Math.floor(points_wanted),
       internal: {
-        points_wanted: Math.floor(points_wanted),
         resampling_group: resampling_group,
         resampling_divisor: resampling_divisor
       }
@@ -256,145 +379,35 @@ export class QueryMonitorModule {
       return obj[0] >= r.after && obj[0] <= r.before;
     });
 
-    this.deleteDimensions();
+    this.deleteDimensionsSetPErcentage();
 
     // Set results
-    const result = [];
-    let currentIntervalMax = r.before;
-    let currentIntervalMin = Math.round(r.before - group * 1000);
-    let currentValues = [];
-    let values_in_group_non_zero = 0;
-    let intervalNotFound;
-
-    let firstEntryReturnedTimestamp;
-    let lastEntryReturnedTimestamp;
-
-    const prepareCurrentValues = () => {
-      let timeToReturn = currentIntervalMax;
-
-      if (!this.options.includes('ms') && !this.options.includes('milliseconds')) timeToReturn /= 1000;
-
-      if (options.includes('nonzero')) {
-        // TODO delete dimensions that have all values to 0
-        // If all dimensions values are 0, return all dimensions
-      }
-
-      if (options.includes('min2max')) {
-        // TODO min2max ???????
-      }
-
-      if (options.includes('null2zero')) {
-        // TODO null2zero ???????
-      }
-
-      if (options.includes('objectrows')) {
-        // TODO
-        /*
-        {
-           "labels": ["time", "guest_nice", "guest", "steal", "softirq", "irq", "user", "system", "nice", "iowait"],
-           "data":[
-            { "time": 1568301750, "guest_nice": 0, "guest": 0, "steal": 0.1538782, "softirq": 2.0604372, "irq": 0, "user": 1.2238275, "system": 14.5363847, "nice": 39.161784, "iowait": 0},
-            { "time": 1568301740, "guest_nice": 0, "guest": 0, "steal": 0.3002563, "softirq": 0.3041003, "irq": 0, "user": 1.3573818, "system": 3.4644972, "nice": 47.999079, "iowait": 0.0502513}
-           ]
-         }
-         */
-      }
-
-      if (options.includes('google_json')) {
-        // TODO [ new Date(2019,8,12,16,23,40), 0, 0, 0.0507614, 0.0510204, 0, 1.3659877, 1.3149827, 0, 0],
-      }
-
-      if (options.includes('percentage')) {
-        // TODO percentage
-      }
-
-      // TODO test unaligned
-
-      if (options.includes('match-ids')) {
-        // TODO match-ids
-      }
-
-      if (options.includes('match-names')) {
-        // TODO match-names
-      }
-
-      if (options.includes('showcustomvars')) {
-        // TODO showcustomvars
-      }
-
-      /**
-       * Set current interval found values
-       */
-      if (currentValues.length === 0) {
-        let resultArray = Array(this.dimensionsNames.length + 1).fill(0); // +1 for time dimension
-        currentValues.push(resultArray);
-      }
-
-      let res;
-
-      /**
-       * Calculate formats
-       */
-      if (this.group === 'average') {
-        res = currentValues.reduce((a, b) => {
-
-          // calculate average
-          return a.map((suba, i) => (a[i] + b[i]) / currentValues.length);
-        });
-        res.shift(); // Delete timestamp average
-      }
-
-      if (!this.latestReturnedValues) this.latestReturnedValues = res.map(Math.abs);
-
-      /**
-       * output formats
-       */
-      if (this.format === 'array') {
-        res = Math.abs(res.reduce((a, b) => a + b));
-        result.push(res);
-
-        if (this.max < res) this.max = res;
-        if (this.min > res) this.min = res;
-      }
-
-      if (this.format === 'json') {
-        if (options.includes('abs') || options.includes('absolute') || options.includes('absolute-sum')) res.map(Math.abs);
-        result.push([timeToReturn, ...res]);
-
-        if (this.max < Math.max(...res)) this.max = Math.max(...res);
-        if (this.min > Math.min(...res)) this.min = Math.min(...res);
-      }
-
-      if (!firstEntryReturnedTimestamp) firstEntryReturnedTimestamp = timeToReturn;
-      lastEntryReturnedTimestamp = timeToReturn;
-
-      // Set next interval and reset current values
-      currentValues = [];
-    };
+    this.currentIntervalMax = r.before;
+    this.currentIntervalMin = Math.round(r.before - group * 1000);
 
     let BreakException = {};
     try {
       this.dbMatchingData.forEach((obj) => {
-        intervalNotFound = true;
+        this.intervalNotFound = true;
 
         // Finish forEach if we already have all points wanted
-        if (result.length >= points_wanted) throw BreakException;
+        if (this.currentResult.length >= r.points_wanted) throw BreakException;
 
         // Keep the loop till the obj matches the interval
-        while(intervalNotFound) {
+        while(this.intervalNotFound) {
           // safe exit
-          if (currentIntervalMin < r.after) intervalNotFound = true;
+          if (this.currentIntervalMin < r.after) return this.intervalNotFound = false;
 
           // Get all objects inside current interval
-          if (obj[0] <= currentIntervalMax && obj[0] > currentIntervalMin && currentValues.length <= r.group) {
-            currentValues.push(obj);
-            intervalNotFound = false;
+          let timestampIndex = (this.options.includes('percentage') ? 1 : 0);
+          if (obj[timestampIndex] <= this.currentIntervalMax && obj[timestampIndex] > this.currentIntervalMin && this.currentValues.length <= r.group) {
+            this.currentValues.push(obj);
+            this.intervalNotFound = false;
           } else {
-            prepareCurrentValues();
+            this.prepareCurrentValues();
 
-            currentIntervalMax = currentIntervalMin;
-            currentIntervalMin -= Math.round((r.update_every / r.group) * 1000);
-            values_in_group_non_zero = 0;
+            this.currentIntervalMax = this.currentIntervalMin;
+            this.currentIntervalMin -= Math.round((r.update_every / r.group) * 1000);
           }
         }
 
@@ -403,13 +416,9 @@ export class QueryMonitorModule {
       if (e !== BreakException) throw e;
     }
 
-    prepareCurrentValues();
+    this.prepareCurrentValues();
 
-    return {
-      firstEntryReturnedTimestamp,
-      lastEntryReturnedTimestamp,
-      result
-    };
+    return this.currentResult;
   }
 
   private convertBeforeAfterToAbsolute(firstEntryTimestamp, lastEntryTimestamp): 1 | 0 {
@@ -504,23 +513,19 @@ export class QueryMonitorModule {
       resultLabels = ['time', ...this.dimensions];
     }
 
-    const result = data.result;
-
     // Always return it in seconds
-    let firstEntryReturnedTimestamp = data.firstEntryReturnedTimestamp;
-    let lastEntryReturnedTimestamp = data.lastEntryReturnedTimestamp;
-    if (this.options.includes('ms') || this.options.includes('milliseconds')) firstEntryReturnedTimestamp /= 1000;
-    if (this.options.includes('ms') || this.options.includes('milliseconds')) lastEntryReturnedTimestamp /= 1000;
+    if (this.options.includes('ms') || this.options.includes('milliseconds')) this.firstEntryReturnedTimestamp /= 1000;
+    if (this.options.includes('ms') || this.options.includes('milliseconds')) this.lastEntryReturnedTimestamp /= 1000;
 
-    if (this.options.includes('flip')) result.reverse();
+    if (this.options.includes('flip')) this.currentResult.reverse();
 
     let resultData;
       if (this.format === 'array') {
-        resultData = result;
+        resultData = this.currentResult;
       } else {
         resultData = {
           labels: resultLabels,
-          data: result
+          data: this.currentResult
         }
     }
 
@@ -535,14 +540,14 @@ export class QueryMonitorModule {
         // One value every this amount of time is kept in the round robin database (indepedently of the current view).'
         first_entry: firstEntryTimestamp, // The UNIX timestamp of the first entry (the oldest) in the round robin database.
         last_entry: lastEntryTimestamp, // The UNIX timestamp of the latest entry in the round robin database.
-        before: lastEntryReturnedTimestamp, // 'The UNIX timestamp of the first entry (the oldest) returned in this response.'
-        after: firstEntryReturnedTimestamp, // 'The UNIX timestamp of the latest entry returned in this response.'
+        before: this.lastEntryReturnedTimestamp, // 'The UNIX timestamp of the first entry (the oldest) returned in this response.'
+        after: this.firstEntryReturnedTimestamp, // 'The UNIX timestamp of the latest entry returned in this response.'
         dimension_names: this.dimensionsNames, // 'The dimension names of the chart as returned in the current view.'
         dimension_ids: this.dimensionsIds, // 'The dimension IDs of the chart as returned in the current view.'
         latest_values: this.latestValues, // 'The latest values collected for the chart (indepedently of the current view).'
         view_latest_values: this.latestReturnedValues, // 'The latest values returned with this response.'
         dimensions: this.dimensionsIds.length, // 'The number of dimensions returned.'
-        points: result.length, // 'The number of rows / points returned.'
+        points: this.currentResult.length, // 'The number of rows / points returned.'
         format: this.format, // 'The format of the result returned.'
         result: resultData, // 'The result requested, in the format requested.'
         min: this.min, // min in all dimensions 'The minimum value returned in the current view. This can be used to size the y-series of the chart.'
@@ -551,7 +556,7 @@ export class QueryMonitorModule {
     } else {
       return {
         labels: resultLabels,
-        data: result
+        data: this.currentResult
       };
     }
 
